d8fe282e5bb8ce6eadd090bad10cf29c938bdb87
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-03-03 15:42:05 +0100

    dish 1

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,7 +1,7 @@
 const Dish = ({ name, price, image }) => {
     return (
       <div className="dish">
-        <img src={image} alt={name} style={{ width: '100px' }} />
+        <img src={image} alt={name} style={{ width: '8rem' }} />
         <h4>{name}</h4>
         <p>Prix: {price}€</p>
       </div>
