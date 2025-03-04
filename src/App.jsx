40e799fd8aa4603e04c23f4772586a7257d5301e
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-03-04 11:29:58 +0100

    activité 7 - 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 import Header from "./components/header";
 import Dish from "./components/Dish";
 import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       <Header />
-      <div className="dish-container">
-        <Dish
-          name="Tacos à l’unité"
-          price={3}
-          image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-        />
-        <Dish
-          name="Enchiladas"
-          price={12}
-          image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-        />
-        <Dish
-          name="Mole poblano"
-          price={15}
-          image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-        />
-      </div>
+      <Container className="py-5">
+        <Row className="justify-content-center mt-4">
+          <Col xs={12} md={6} lg={4}>
+            <Dish
+              name="Tacos à l’unité"
+              price={3}
+              image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <Dish
+              name="Enchiladas"
+              price={12}
+              image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
+            />
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <Dish
+              name="Mole poblano"
+              price={15}
+              image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
+            />
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
-import { Navbar } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
 import "../header.scss";
 
 function Header() {
   return (
     <header>
-      <Navbar bg="light" padding="2">
-        <img
-          className="logo"
-          src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-
-3262834_960_720.png"
-          alt="Mexicain avec un chapeau qui joue de la guitare."
-        />
-        <nav className="ms-auto">
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand>
+            <img
+              className="logo"
+              src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+              alt="Mexicain avec un chapeau qui joue de la guitare."
+              height="50"
+            />
+          </Navbar.Brand>
           <a href="index.html">Accueil</a>
-        </nav>
+        </Container>
       </Navbar>
     </header>
   );
