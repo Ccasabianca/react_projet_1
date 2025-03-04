7f2ae303113abed9cba360ad23ef364d777dcc7c
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-03-04 14:56:16 +0100

    activité 9 - 1

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,9 +1,16 @@
-import { Card } from "react-bootstrap";
+import { Card, Badge } from "react-bootstrap";
 import "../dish.scss";
 
-function Dish({ name, price, image }) {
+function Dish({ name, price, image, isNew }) {
   return (
-    <Card className="dish">
+    <Card className="dish position-relative">
+      {isNew && (
+        <Badge 
+          bg="primary" 
+          className="position-absolute top-0 end-0 m-2">
+          Nouveau
+        </Badge>
+      )}
       <Card.Img variant="top" src={image} alt={name} className="dish-img" />
       <Card.Body>
         <Card.Text>
